bb09dbfedb320c75765f134dc5797e454bd32339
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2024-02-15 23:50:26 +0800

    Redesigned the form

--- problem2/src/components/ExchangeForm/ExchangeForm.js
+++ problem2/src/components/ExchangeForm/ExchangeForm.js
@@ -1,16 +1,42 @@
 import { useEffect, useState } from 'react';
+import TokenPopup from '../TokenPopup/TokenPopup';
 import './ExchangeForm.css';
 
+function importAll(r) {
+    let images = {};
+    r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+const images = importAll(require.context('../../assets/images/tokens', false, /\.svg/));
+
 const ExchangeForm = () => {
-    const handleSubmit = (e) => {};
     const [prices, setPrices] = useState([]);
     const [inAmount, setInAmount] = useState(0);
-    const [inToken, setInToken] = useState('ETH');
+    const [inToken, setInToken] = useState('');
     const [outAmount, setOutAmount] = useState(0);
     const [outToken, setOutToken] = useState('');
+    const [inPopup, setInPopup] = useState(false);
+    const [outPopup, setOutPopup] = useState(false);
+    const [error, setError] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setError("");
+        setMessage("");
+        if (!(inAmount && outAmount)) {
+            setError("You cannot exchange zero tokens");
+        } else {
+            setMessage("Exchange successful!");
+            setInAmount(0);
+            setOutAmount(0);
+            setInToken('');
+            setOutToken('');
+        }
+    };
 
     const computeExchange = (amount, inCurrency, outCurrency) => {
-        if (!(inCurrency && outCurrency)) {
+        if (!(inCurrency && outCurrency)) { 
             return 0;
         }
         const inPrice = prices.find(element => element.currency === inCurrency).price;
@@ -19,6 +45,14 @@ const ExchangeForm = () => {
         return amount * inPrice / outPrice;
     }
 
+    const getPrice = (token, amount) => {
+        const data = prices.find(entry => entry.currency === token);
+        if (data && amount) {
+            return data.price * amount;
+        } else {
+            return 0;
+        }
+    }
     // Fetch prices data to compute exchanges
     useEffect(() => {
         const fetchPrices = async () => {
@@ -33,42 +67,96 @@ const ExchangeForm = () => {
         fetchPrices();
     }, [])
 
+    useEffect(() => {
+        setOutAmount(computeExchange(inAmount, inToken, outToken));
+        console.log('boo');
+    }, [inToken, outToken])
+
     return (
         <div className="exchange-form">
-            <form onSubmit={handleSubmit}>
-                <h2>Swap</h2>
+            {inPopup && <TokenPopup tokenState={setInToken} popupState={setInPopup} tokens={prices.map(entry => entry.currency)}></TokenPopup>}
+            {outPopup && <TokenPopup tokenState={setOutToken} popupState={setOutPopup} tokens={prices.map(entry => entry.currency)}></TokenPopup>}
+            <div className="wrapper">
+                <form onSubmit={handleSubmit}>
+                    <h2>Token Exchange</h2>
 
-                <label htmlFor="input-token">Choose token</label>
-                <select name="input-token" id="input-token" value={inToken} 
-                onChange={e => {setInToken(e.target.value);
-                                setOutAmount(computeExchange(inAmount, inToken, e.target.value))}}>
-                    {prices && prices.map(entry => 
-                        <option value={entry.currency}>{entry.currency}</option>
-                    )}
-                </select>
+                    {/* <label htmlFor="input-token">Choose token</label>
+                    <select name="input-token" id="input-token" value={inToken} 
+                    onChange={e => {setInToken(e.target.value)
+                                    setOutAmount(computeExchange(inAmount, e.target.value, outToken))}}>
+                        {prices && prices.map(entry => 
+                            <option value={entry.currency}>{entry.currency}</option>
+                        )}
+                    </select> */}
 
-                <label htmlFor="input-amount">Amount to send</label>
-                <input type="number" name="input-amount" id="input-amount" value={inAmount} 
-                onChange={e => {setInAmount(e.target.value);
-                                setOutAmount(computeExchange(e.target.value, inToken, outToken));}}/>
+                    
+                    <div className='form-box'>
+                        <label htmlFor="input-amount">Send</label>
+                        <div className='input-fields'>
+                            <input type="number" 
+                                min="0"
+                                step="any"
+                                name="input-amount" 
+                                id="input-amount"
+                                placeholder='0' 
+                                value={inAmount} 
+                                onChange={e => {setInAmount(e.target.value);
+                                                setOutAmount(computeExchange(e.target.value, inToken, outToken))}}/>
+                            
+                            {!inToken && <p className='token-select-false' onClick={e => setInPopup(true)}>Select Token <span class="material-symbols-outlined">expand_more</span></p>}
+                            {inToken && <p className='token-select-true' onClick={e => setInPopup(true)}>
+                                <img src={images[inToken + ".svg"]} alt="" />
+                                <p>{inToken}</p>
+                                <span class="material-symbols-outlined">expand_more</span>
+                                </p>}
+                        </div>
+                        <div className='price'>${getPrice(inToken, inAmount)}</div>
+                    </div>
 
-                <label htmlFor="output-token">Choose token</label>
-                <select name="output-token" id="output-token" value={outToken} 
-                onChange={e => {setOutToken(e.target.value);
-                                setOutAmount(computeExchange(inAmount, inToken, e.target.value));}}>
-                    <option value=''/>
-                    {prices && prices.map(entry => 
-                        <option value={entry.currency}>{entry.currency}</option>
-                    )}
-                </select>
+                    <span class="material-symbols-outlined swap-icon" onClick={e => {
+                        const temp = inToken;
+                        setInToken(outToken);
+                        setOutToken(temp);
+                    }}> swap_vert </span>
 
-                <label htmlFor="input-amount">Amount to receive</label>
-                <input type="number" name="output-amount" id="output-amount" value={outAmount} 
-                onChange={e => {setOutAmount(e.target.value);
-                                setInAmount(computeExchange(e.target.value, outToken, inToken))}}/>
+                    <div className='form-box'>
+                        <label htmlFor="output-amount">Receive</label>
+                        <div className='input-fields'>
+                            <input type="number" 
+                                min="0"
+                                step="any"
+                                name="output-amount" 
+                                id="output-amount" 
+                                placeholder='0'
+                                value={outAmount} 
+                                onChange={e => {setOutAmount(e.target.value);
+                                                setInAmount(computeExchange(e.target.value, outToken, inToken))}}/>
 
-                <button>CONFIRM SWAP</button>
-            </form>
+                            {!outToken && <p className='token-select-false' onClick={e => setOutPopup(true)}>Select Token <span class="material-symbols-outlined">expand_more</span></p>}
+                            {outToken && <p className='token-select-true' onClick={e => setOutPopup(true)}>
+                                <img src={images[outToken + ".svg"]} alt="" />
+                                <p>{outToken}</p>
+                                <span class="material-symbols-outlined">expand_more</span>
+                                </p>}
+                        </div>
+                        <div className='price'>${getPrice(outToken, outAmount)}</div>
+                    </div>
+                    {/* <label htmlFor="output-token">Choose token</label>
+                    <select name="output-token" id="output-token" value={outToken} 
+                    onChange={e => {setOutToken(e.target.value)
+                                    setOutAmount(computeExchange(inAmount, inToken, e.target.value))}}>
+                        <option value=''/>
+                        {prices && prices.map(entry => 
+                            <option value={entry.currency}>{entry.currency}</option>
+                        )}
+                    </select> */}
+
+                    
+                   {error && <div className="error"><p>{error}</p></div>}
+                   {message && <div className="message"><p>{message}</p></div>}
+                    <button>Confirm Swap</button>
+                </form>
+            </div>
         </div>
     )
 }
---
+++ problem2/src/components/TokenPopup/TokenPopup.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import './TokenPopup.css'
+function importAll(r) {
+    let images = {};
+    r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
+    return images;
+}
+const images = importAll(require.context('../../assets/images/tokens', false, /\.svg/));
+console.log(images);
+const TokenPopup = (props) => {
+    const setToken = props.tokenState;
+    const setPopup = props.popupState;
+    const tokens = props.tokens;
+    const [searchedToken, setSearchedToken] = useState('');
+    const [filteredTokens, setFilteredTokens] = useState([...tokens]);
+
+    const handleChange = (e) => {
+        const search = e.target.value;
+        setSearchedToken(e.target.value);
+
+        const filtered = tokens.filter((token) => token.toLowerCase().includes(search.toLowerCase()));
+        setFilteredTokens(filtered);
+    }
+
+    return (
+        <div className="token-popup-wrapper" onClick={(e) => (e.target.className === "token-popup-wrapper") && setPopup(false)}>
+            <div className="token-popup">
+                <div className="header">
+                    <h2>Select token</h2>
+                    <span class="material-symbols-outlined close" onClick={e => setPopup(false)}>close</span>
+                </div>
+                <div className="searchbar">
+                    <span class="material-symbols-outlined">search</span>
+                    <input type="text" name="search" id="search" placeholder="Search" value={searchedToken} onChange={handleChange}/>
+                </div>
+                <h3>Search results</h3>
+                <div className="tokens-list">
+                    {filteredTokens && filteredTokens.map(token => 
+                    <div className='token' onClick={e => {setToken(token); setPopup(false)}}><img src={images[token + ".svg"]} alt="" /><p>{token}</p></div>)}
+                    {!filteredTokens.length && <div className='no-token'>No tokens found!</div>}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TokenPopup;

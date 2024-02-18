d2cd8f0145e011e40dc9693323768ae226dfec68
ty4g1 <126762501+ty4g1@users.noreply.github.com>
2024-02-18 13:42:21 +0800

    Cleanup and comments

--- problem2/src/components/ExchangeForm/ExchangeForm.js
+++ problem2/src/components/ExchangeForm/ExchangeForm.js
@@ -5,6 +5,7 @@ import './ExchangeForm.css'
 import './ExchangeForm-light.css';
 import './ExchangeForm-dark.css';
 
+// Importing svg images for tokens
 function importAll(r) {
     let images = {};
     r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
@@ -13,26 +14,43 @@ function importAll(r) {
 const images = importAll(require.context('../../assets/images/tokens', false, /\.svg/));
 
 const ExchangeForm = () => {
+    //Initializing prices state
     const [prices, setPrices] = useState([]);
+    
+    // Initializing states for input fields
     const [inAmount, setInAmount] = useState();
     const [inToken, setInToken] = useState('');
     const [outAmount, setOutAmount] = useState();
     const [outToken, setOutToken] = useState('');
+
+    // Initializing popup states
     const [inPopup, setInPopup] = useState(false);
     const [outPopup, setOutPopup] = useState(false);
+
+    //Initializing error and message states
     const [error, setError] = useState('');
     const [message, setMessage] = useState('');
     
-
+    // Handling Submission of exchangeForm
     const handleSubmit = (e) => {
+
+        // Prevent refresh
         e.preventDefault();
+
+        // Reset error and message
         setError("");
         setMessage("");
+
+        // Check for selected tokens and set error accordingly
         if (!(inToken && outToken)) {
             setError("Please select a token");
-        } else if (!(inAmount && outAmount)) {
+        }
+        // Check for non-zero amount and set error accordingly
+        else if (!(inAmount && outAmount)) {
             setError("You cannot exchange zero tokens");
-        } else {
+        } 
+        // Update message and reset input fields 
+        else {
             setMessage("Exchange successful!");
             setInAmount('');
             setOutAmount('');
@@ -41,6 +59,7 @@ const ExchangeForm = () => {
         }
     };
 
+    // Function to calculate exchange value using prices array
     const computeExchange = (amount, inCurrency, outCurrency) => {
         if (!(inCurrency && outCurrency)) { 
             return 0;
@@ -51,6 +70,7 @@ const ExchangeForm = () => {
         return amount * inPrice / outPrice;
     }
 
+    // Funtion to get the price (in USD) for a given token
     const getPrice = (token, amount) => {
         const data = prices.find(entry => entry.currency === token);
         if (data && amount) {
@@ -59,7 +79,8 @@ const ExchangeForm = () => {
             return 0;
         }
     }
-    // Fetch prices data to compute exchanges
+
+    // Fetch prices data using the API endpoint provided, whenever the page is rendered
     useEffect(() => {
         const fetchPrices = async () => {
             const response = await fetch('https://interview.switcheo.com/prices.json');
@@ -73,6 +94,7 @@ const ExchangeForm = () => {
         fetchPrices();
     }, [])
 
+    // Recompute exchange amounts only when selected tokens are changed
     useMemo(() => {
         if (inToken && outToken) {
             setOutAmount(computeExchange(inAmount, inToken, outToken));
@@ -81,14 +103,22 @@ const ExchangeForm = () => {
 
     return (
         <div className="exchange-form">
+
+            {/* Button to toggle light or dark mode */}
             <ThemeToggleButton/>
+
+            {/* The popup/modal component to select (in or out) token */}
             {inPopup && <TokenPopup tokenState={setInToken} popupState={setInPopup} tokens={prices.map(entry => entry.currency)}></TokenPopup>}
             {outPopup && <TokenPopup tokenState={setOutToken} popupState={setOutPopup} tokens={prices.map(entry => entry.currency)}></TokenPopup>}
+            
+            {/* wrapper div containing the exchange form */}
             <div className="wrapper">
                 <h2>Token Exchange</h2>
                 <form onSubmit={handleSubmit}>
+                    {/* div containing label, input, select token button and price display */}
                     <div className='form-box'>
                         <label htmlFor="input-amount">Send</label>
+                        {/* Input field allowing only positive numeric input (including float numbers) */}
                         <div className='input-fields'>
                             <input type="number" 
                                 min="0"
@@ -98,18 +128,26 @@ const ExchangeForm = () => {
                                 placeholder='0' 
                                 autoFocus
                                 value={inAmount} 
+                                // Compute exchange amount output on input change
                                 onChange={e => {setInAmount(e.target.value);
                                                 if (inToken && outToken) {
                                                     setOutAmount(computeExchange(e.target.value, inToken, outToken));
                                                 }}}/>
-                            
+                           
+                            {/* If no token has been selected, display select token button, display popup on click */}
                             {!inToken && <p className='token-select-false' onClick={e => setInPopup(true)}>Select Token <span class="material-symbols-outlined">expand_more</span></p>}
+                            
+                            {/* If token has been selected, display token name and icon, 
+                            while still being a token select button and display popup on click */}
                             {inToken && <p className='token-select-true' onClick={e => setInPopup(true)}>
                                 <img src={images[inToken + ".svg"]} alt="" />
                                 <p>{inToken}</p>
                                 <span class="material-symbols-outlined">expand_more</span>
                                 </p>}
+
                         </div>
+                        
+                        {/* Display price of selected token's amount */}
                         <div className='price'>${getPrice(inToken, inAmount)}</div>
                     </div>
 
@@ -119,6 +157,7 @@ const ExchangeForm = () => {
                         setOutToken(temp);
                     }}> swap_vert </span>
 
+                    {/* Same as input wrapper, modifying different states (for output field now)  */}
                     <div className='form-box'>
                         <label htmlFor="output-amount">Receive</label>
                         <div className='input-fields'>
@@ -143,8 +182,12 @@ const ExchangeForm = () => {
                         </div>
                         <div className='price'>${getPrice(outToken, outAmount)}</div>
                     </div>
+                    
+                    {/* Button to submit form */}
                     <button>Exchange</button>
                 </form>
+                
+                {/* Error and message divs to display error or success to user */}
                 {error && <div className="error"><p>{error}</p></div>}
                 {message && <div className="message"><p>{message}</p></div>}
             </div>
--- problem2/src/components/TokenPopup/TokenPopup.js
+++ problem2/src/components/TokenPopup/TokenPopup.js
@@ -2,20 +2,30 @@ import { useState } from 'react';
 import './TokenPopup.css'
 import './TokenPopup-light.css'
 import './TokenPopup-dark.css'
+
+// Importing svg images for tokens
 function importAll(r) {
     let images = {};
     r.keys().map((item) => { images[item.replace('./', '')] = r(item); });
     return images;
 }
 const images = importAll(require.context('../../assets/images/tokens', false, /\.svg/));
-console.log(images);
+
+
 const TokenPopup = (props) => {
+    // Accessing props to modify states of popup and tokens
     const setToken = props.tokenState;
     const setPopup = props.popupState;
+
+    // Accessing list of tokens
     const tokens = props.tokens;
+
+    // Setting states for searching and filtering tokens
     const [searchedToken, setSearchedToken] = useState('');
     const [filteredTokens, setFilteredTokens] = useState([...tokens]);
 
+    // Function to handle change in search bar input
+    // Looks for searched token and modifies searchedToken and filteredTokens states
     const handleChange = (e) => {
         const search = e.target.value;
         setSearchedToken(e.target.value);
@@ -25,22 +35,31 @@ const TokenPopup = (props) => {
     }
 
     return (
+        // Wrapper div for the popop/modal. Closes the popup when clicked on
         <div className="token-popup-wrapper" onClick={(e) => (e.target.className === "token-popup-wrapper") && setPopup(false)}>
             <div className="token-popup">
+
+                {/* Header containing h2 and close icon, which closes the popup whenc clicked */}
                 <div className="header">
                     <h2>Select token</h2>
                     <span class="material-symbols-outlined close" onClick={e => setPopup(false)}>close</span>
                 </div>
+
+                {/* Searchbar div containing search icon and input field. Auto updates token list on input change */}
                 <div className="searchbar">
                     <span class="material-symbols-outlined">search</span>
                     <input type="text" name="search" id="search" autoFocus placeholder="Search" value={searchedToken} onChange={handleChange}/>
                 </div>
+
                 <h3>Search results</h3>
+
+                {/* Displays all filtered tokens matching the search term. Displays "No Tokens found!" if there are none */}
                 <div className="tokens-list">
                     {filteredTokens && filteredTokens.map(token => 
                     <div className='token' onClick={e => {setToken(token); setPopup(false)}}><img src={images[token + ".svg"]} alt="" /><p>{token}</p></div>)}
                     {!filteredTokens.length && <div className='no-token'>No tokens found!</div>}
                 </div>
+                
             </div>
         </div>
     )
